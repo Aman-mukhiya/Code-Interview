3b3af0783c58ff5740e616cd82b6bd3096c77469
Aman Mukhiya <amanmukhiya4@gmail.com>
2025-11-10 20:03:48 +0545

    fix: correct variable name typos and ensure proper function references in Inngest setup

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -2,26 +2,26 @@ import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
 
-export const inggest = new Inngest({ id: "code-interview" });
+export const inngest = new Inngest({ id: "code-interview" });
     //1:56:00
-const syncUser = inggest.createFunction(
+const syncUser = inngest.createFunction(
     {id:"sync-user"},
     {event:"clerk/user.created"},
     async({event}) => {
         await connectDB()
-        const {id, email_addresses, first_name, last_name, imgae_url} = event.data
+        const {id, email_addresses, first_name, last_name, image_url} = event.data
         const newUser = {
             clerkId: id,
             email:email_addresses[0]?.email_address,
             name:`${first_name || ""} ${last_name || ""}`,
-            profileImage:imgae_url
+            profileImage:image_url
         }
         await User.create(newUser)
     }
 )
 
-const deleteUserFromDB = inggest.createFunction(
-    {id:"sync-user"},
+const deleteUserFromDB = inngest.createFunction(
+    {id:"delete-user-from-db"},
     {event:"clerk/user.deleted"},
 
     async({event}) => {
